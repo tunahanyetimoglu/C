2bae367dc062e6d5f4149eccf8dac37041ad0ad7
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-04-04 22:43:48 +0300

    Create main.c

---
+++ Deneme/main.c
@@ -0,0 +1,17 @@
+#include "kutuphane.h"
+
+
+int main(void){
+
+  generateStudentgrades();     
+  printStudentgrades();
+  puts("Passed");
+  passedStudent();
+  puts("-----------");
+  changeGrades();
+  puts("After change");
+  printStudentgrades();
+  puts("passed");
+  passedStudent();
+  
+} 
