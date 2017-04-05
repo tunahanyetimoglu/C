b555781a06a145bdede0df426a5287313b155df3
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-04-05 09:28:04 +0300

    Update random_10numbers.c

--- Introduction to Programming/random_10numbers.c
+++ Introduction to Programming/random_10numbers.c
@@ -5,12 +5,11 @@
 int main () {
 
    int n[ 10 ]; /* n is an array of 10 integers */
-   int i,j;
-    srand(time(NULL));
+   srand(time(NULL));
    /* initialize elements of array n to 0 */         
-   for ( i = 0; i < 10; i++ ) {
+   for (int i = 0; i < 10; i++ ) {
       n[ i ] = rand() %100;
-       printf("Element[%d] = %d\n", i+1, n[i] ); /* set element at location i to i + 100 */
+      printf("Element[%d] = %d\n", i+1, n[i] ); /* set element at location i to i + 100 */
    }
  
    return 0;
